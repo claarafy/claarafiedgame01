0f23e1ac228fcee0b4fadf86584dc07d90e969cb
Clara Kang <clara.kang91@gmail.com>
2017-04-03 12:14:45 -0700

    added a container div inside the body (to better control the game) and added a start button function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var $body = $('body');
+var $container = $('#container');
 var $titleButton = $('#title-button');
 var $title = $('#title-heading');
 var $titlePage = $('#title-page');
@@ -7,10 +8,14 @@ var $p02Name = $("input[name='p02-name']");
 
 
 //when tittle button is clicked, redo the titles
-$titleButton.on('click', clearAll, redoTitle);
+$titleButton.on('click', function() {
+  redoTitle();
+});
 
-function clearAll () { //clear everything in the body
-  $body.empty();
+function clearAll() { //clear everything in the body
+  $('*').off()
+  $container.empty();
+  $('*').off()
 }
 
 function redoTitle() {
@@ -19,10 +24,19 @@ function redoTitle() {
   $titleButton.remove();
   $('#p01-input').text('Player 1:  ' + $p01Name.val() );
   $('#p02-input').text('Player 2:  ' + $p02Name.val() );
-  $body.append('<div id="how-box"><button id="how">How to Play</button></div>');
+  $container.append('<div id="how-box"><button id="how">How to Play</button></div>');
   $('#how').one('click', showIns);
 }
-
+//when how to play button is clicked show Instructions
 function showIns() {
-  $body.append("<div id='instructions'><p>Instructions</p><ol><li>Remember!!</li><li>Wearing what?</li><li>Holding what?</li><li>Standing how?</li><li>Where?</li><li>Read the mind!</li><li>Name it!</li><ol></div>");
+  $container.append("<div id='instructions'><p>Instructions</p><ol><li>Remember!!</li><li>Wearing what?</li><li>Holding what?</li><li>Standing how?</li><li>Where?</li><li>Read the mind!</li><li>Name it!</li><ol></div>");
+  $container.append("<div id='start-box'><button id='start'>START</button></div>");
+  $('#start').on('click', function() {
+  clearAll();
+  startGame();
+  });
+}
+//Main picture shown
+function startGame() {
+  console.log("game started");
 }
