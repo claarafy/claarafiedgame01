3de89240f37802564dd71aa7864958881293d1d1
Clara Kang <clara.kang91@gmail.com>
2017-04-02 18:20:14 -0700

    resizing the title heading and button with a click of the start button

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-var $title_button = $("#title-button");
+var $title_button = $('#title-button');
+var $title = $('#title');
+var $title_page = $('#title-page');
+
+$title_button.on('click', redoTitle);
+
+function redoTitle() {
+  $title.css({fontSize: '40px'});
+  $title_page.css({marginTop: '10px'})
+  $title_button.remove();
+}
