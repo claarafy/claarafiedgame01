1ec0a4fecd11cf15627e15ac48615e19c5966c7a
Clara Kang <clara.kang91@gmail.com>
2017-04-04 19:14:19 -0700

    first round: added head options and check correct head function

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 var $body = $('body');
-var $container = $('#container');
+var $container = $('.container');
 var $titleButton = $('#title-button');
 var $title = $('#title-heading');
 var $titlePage = $('#title-page');
 var $p01Name = $("input[name='p01-name']");
 var $p02Name = $("input[name='p02-name']");
 var timer;
+var game = {
+  player01: {name: "", score: 0},
+  player02: {name: "", score: 0},
+  time: 15
+}
+var currentPlayer = game.player01;
 
-//when tittle button is clicked, redo the titles
+//when "jQuery link" button is clicked, redo the titles
 $titleButton.on('click', function() {
   redoTitle();
 });
@@ -27,28 +33,76 @@ function redoTitle() {
   $container.append("<div id='how-box'><button id='how'>How to Play</button></div>");
   $('#how').one('click', showIns);
 }
-//when how to play button is clicked show Instructions
+//when "how to play button" is clicked show Instructions
 function showIns() {
   $container.append("<div id='instructions'><p>Instructions</p><ol><li>Remember!!</li><li>Wearing what?</li><li>Holding what?</li><li>Standing how?</li><li>Where?</li><li>Read the mind!</li><li>Name it!</li><ol></div>");
   $container.append("<div id='start-box'><button id='start'>START</button></div>");
   $('#start').on('click', function() {
     clearAll();
     startGame();
-    timer = 3;
-    setInterval(countdown, 1000);
   });
 }
 //when start button clicked, expose main picture
 function startGame() {
+  clearAll();
   $('#main').removeClass('hidden');//show the picture
+  countdown();
 };
 //countdown
 function countdown() {
-  if (timer == 0) {
-    $('.timer').text('Time over!');
-    $('#main').addClass('hidden'); //when timer hits 0, hide the #main
-  }
-  else {
-  $('.timer').text('Time: ' + (timer -=  1));
-  }
+  timer = 3;
+  var startCountdown = setInterval(function() {
+    if (timer == 0) {
+      $('.timer01').text('Time over!');
+      $('#main').addClass('hidden'); //when timer hits 0, hide the #main
+      clearInterval(startCountdown);
+      firstRound();
+    }
+    else {
+    $('.timer01').text('Time: ' + (timer -=  1));
+    }
+  }, 1000);
+}
+
+function firstRound() {
+  $('#first').removeClass('hidden');
+  player02Turn();
+  displayHeads();
+  checkHeads();
+}
+
+//who's turn is it
+function player01Turn() {
+  currentPlayer = $p01Name.val();
+  $('#p01-input').css({fontSize: '40px', color: 'tomato', fontWeight: '800'});
+}
+function player02Turn() {
+  currentPlayer = $p02Name.val();
+  $('#p02-input').css({fontSize: '40px', color: 'tomato', fontWeight: '800'});
+}
+
+var correct = [
+    { name:'head', img: "Head_Correct.png" },
+    { name:'face', img: "Face_Correct.png"},
+    { name:'upper', img: "Upper_Correct.png"},
+    { name:'lower', img: "Lower_Correct.png"},
+    { name:'feet', img: "Feet_Correct.png"}
+  ];
+function displayHeads() {
+  $('#first').append('<div id="correct-head" class="head-option"><img src="Head_Correct.png"</div>');
+  $('#first').append('<div class="head-option"><img src="Head_01.png"</div>');
+  $('#first').append('<div class="head-option"><img src="Head_02.png"</div>');
+  $('#first').append('<div class="head-option"><img src="Head_03.png"</div>');
+  $('#first').append('<div class="head-option"><img src="Head_04.png"</div>');
+  $('#first').append('<div class="head-option"><img src="Head_05.png"</div>');
+}
+
+function checkHeads() {
+  $('.head-option').on('click', function() {
+    if ($(this).children().attr('src') == correct[0].img) {
+      alert('CORRECTTT!!!')
+    } else {
+      alert('NOOOOOO!')
+    }
+  })
 }
